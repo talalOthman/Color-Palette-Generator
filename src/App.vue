0a0e141f3872129dfe440149cf5812c5da7f66a1
Talal <talalothmanse@gmail.com>
2023-03-19 01:24:26 +0800

    -ColorCard component design is production ready

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import axios from 'axios'
+import ColorCard from './components/ColorCard.vue'
 </script>
 
 <template>
-<button class="btn w-64 rounded-full">Button</button>
+  <div class="flex flex-col md:flex-row md:gap-x-4 items-center justify-center min-h-screen w-screen gap-y-4 p-3">
+    <ColorCard />
+    <ColorCard />
+    <ColorCard />
+    <ColorCard />
+    <ColorCard />
+  </div>
 </template>
 
---
+++ src/components/ColorCard.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="border flex flex-col items-center h-64 w-40 p-1 rounded-md bg-white text-gray-600">
+        <div class="bg-purple-600 h-60 w-full rounded-md"></div>
+        <div class="flex items-center justify-center w-full h-10">
+            <p>#ECA400</p>
+        </div>
+    </div>
+</template>
