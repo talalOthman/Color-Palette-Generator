a4513b5610d1072609780ec6d3979c054f7355a9
Talal <talalothmanse@gmail.com>
2023-03-19 04:38:27 +0800

    -Fixed issues with cursor

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ const alertUser = () => {
 <template>
   <div class="flex flex-col md:gap-x-4 items-center justify-evenly min-h-screen w-screen gap-y-4 p-3">
     <Alert v-if="isAlert" />
-    <h1 class="font-bold text-3xl md:text-5xl">Color Pallete Generator</h1>
+    <h1 class="cursor-default font-bold text-3xl md:text-5xl">Color Pallete Generator</h1>
     <div class="flex flex-col justify-center items-center gap-y-4 md:flex-row md:gap-x-3">
       <div class="flex gap-x-3">
         <ColorCard @alert-event="alertUser"/>
@@ -31,7 +31,7 @@ const alertUser = () => {
     </div>
     <div class="flex flex-col items-center gap-y-3">
       <button class="btn btn-wide btn-primary">Generate Pallete</button>
-      <p class="hidden md:block">Or just press the "Spacebar" to generate new palettes.</p>
+      <p class="cursor-default hidden md:block">Or just press the "Spacebar" to generate new palettes.</p>
     </div>
     <Message />
   </div>
--- src/components/ColorCard.vue
+++ src/components/ColorCard.vue
@@ -3,7 +3,7 @@ defineEmits(['alertEvent'])
 </script>
 
 <template>
-    <div @click="$emit('alertEvent')" class="border flex flex-col items-center h-40 w-28 md:h-64 md:w-40 p-1 rounded-md bg-white text-gray-600">
+    <div @click="$emit('alertEvent')" class="cursor-pointer border flex flex-col items-center h-40 w-28 md:h-64 md:w-40 p-1 rounded-md bg-white text-gray-600">
         <div class="bg-purple-600 h-60 w-full rounded-md"></div>
         <div class="flex items-center justify-center w-full h-10">
             <p>#ECA400</p>
