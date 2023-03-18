b6ca7a0188b5dafab504a34df3ef428a77f9ca47
Talal <talalothmanse@gmail.com>
2023-03-19 03:34:19 +0800

    -Updated the layout

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,23 @@ import ColorCard from './components/ColorCard.vue'
 </script>
 
 <template>
-  <div class="flex flex-col md:flex-row md:gap-x-4 items-center justify-center min-h-screen w-screen gap-y-4 p-3">
-    <ColorCard />
-    <ColorCard />
-    <ColorCard />
-    <ColorCard />
-    <ColorCard />
+  <div class="flex flex-col md:gap-x-4 items-center justify-evenly min-h-screen w-screen gap-y-4 p-3">
+    <h1 class="font-bold text-3xl md:text-5xl">Color Pallete Generator</h1>
+    <div class="flex flex-col justify-center items-center gap-y-4 md:flex-row md:gap-x-3">
+    <div class="flex gap-x-3">
+      <ColorCard />
+      <ColorCard />
+      <ColorCard />
+    </div>
+    <div class="flex gap-x-3">
+      <ColorCard />
+      <ColorCard />
+    </div>
+  </div>
+  <div class="flex flex-col items-center gap-y-3">
+    <button class="btn btn-wide btn-primary">Generate Pallete</button>
+    <p class="hidden md:block">Or just press the "Spacebar" to generate new palettes.</p>
+  </div>
   </div>
 </template>
 
