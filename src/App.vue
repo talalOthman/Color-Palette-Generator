26a2bf645e77c885fce53f773908736d3282f4a4
Talal <talalothmanse@gmail.com>
2023-03-20 00:07:14 +0800

    -Data is fetched from external api

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,40 @@
 import ColorCard from './components/ColorCard.vue'
 import Message from './components/Message.vue'
 import Alert from './components/Alert.vue'
-import { ref } from 'vue'
+import { onMounted, computed, ref } from 'vue'
+import axios from 'axios'
+import Spinner from './components/Spinner.vue'
 
+const URL = 'http://colormind.io/api/'
 const isAlert = ref(false)
+const colors = ref([])
+const isLoading = ref(true)
+const hexColors = computed(() => colors.value.map(color => rgbToHex(...color)))
+
+
+onMounted(() => {
+  onGenerate()
+  isLoading.value = true
+})
+
+const onGenerate = async () => {
+  isLoading.value = true
+  const response = await axios.post(URL, {
+    model: 'default'
+  }, {
+    headers: {
+      'Content-Type': 'text/plain'
+    }
+  })
+  colors.value = response.data.result
+  isLoading.value = false
+}
+
+const rgbToHex = (r, g, b) => {
+  let hex = ((r << 16) | (g << 8) | b).toString(16);
+  return "#" + "0".repeat(6 - hex.length) + hex;
+}
+
 const alertUser = () => {
   isAlert.value = true
 
@@ -15,22 +46,25 @@ const alertUser = () => {
 </script>
 
 <template>
-  <div class="flex flex-col md:gap-x-4 items-center justify-evenly min-h-screen w-screen gap-y-4 p-3">
+  <div v-if="isLoading" class="flex min-h-screen justify-center items-center">
+    <Spinner />
+  </div>
+  <div v-else class="flex flex-col md:gap-x-4 items-center justify-evenly min-h-screen w-screen gap-y-4 p-3">
     <Alert v-if="isAlert" />
     <h1 class="cursor-default font-bold text-3xl md:text-5xl">Color Pallete Generator</h1>
     <div class="flex flex-col justify-center items-center gap-y-4 md:flex-row md:gap-x-3">
       <div class="flex gap-x-3">
-        <ColorCard @alert-event="alertUser"/>
-        <ColorCard @alert-event="alertUser"/>
-        <ColorCard @alert-event="alertUser"/>
+        <ColorCard @alert-event="alertUser" :color="hexColors[0].toUpperCase()"/>
+        <ColorCard @alert-event="alertUser" :color="hexColors[1].toUpperCase()"/>
+        <ColorCard @alert-event="alertUser" :color="hexColors[2].toUpperCase()"/>
       </div>
       <div class="flex gap-x-3">
-        <ColorCard @alert-event="alertUser"/>
-        <ColorCard @alert-event="alertUser"/>
+        <ColorCard @alert-event="alertUser" :color="hexColors[3].toUpperCase()"/>
+        <ColorCard @alert-event="alertUser" :color="hexColors[4].toUpperCase()"/>
       </div>
     </div>
     <div class="flex flex-col items-center gap-y-3">
-      <button class="btn btn-wide btn-primary">Generate Pallete</button>
+      <button @click="onGenerate" class="btn btn-wide btn-primary">Generate Pallete</button>
       <p class="cursor-default hidden md:block">Or just press the "Spacebar" to generate new palettes.</p>
     </div>
     <Message />
--- src/components/ColorCard.vue
+++ src/components/ColorCard.vue
@@ -1,12 +1,13 @@
 <script setup>
 defineEmits(['alertEvent'])
+defineProps(['color'])
 </script>
 
 <template>
     <div @click="$emit('alertEvent')" class="cursor-pointer border flex flex-col items-center h-40 w-28 md:h-64 md:w-40 p-1 rounded-md bg-white text-gray-600">
-        <div class="bg-purple-600 h-60 w-full rounded-md"></div>
+        <div class="bg-purple-600 h-60 w-full rounded-md" :style="{ 'background-color': color }"></div>
         <div class="flex items-center justify-center w-full h-10">
-            <p>#ECA400</p>
+            <p class="font-bold">{{ color }}</p>
         </div>
     </div>
 </template>
